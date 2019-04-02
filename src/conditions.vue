7f540bb68e1ca0d0b3a09f3582cde19d4fabd590
Vladimir Nickolaev <exlnted@gmail.com>
2019-04-02 14:20:00 +0300

    refactoring

--- src/conditions.vue
+++ src/conditions.vue
@@ -48,10 +48,10 @@ export default{
     },
     swap() {
       this.textNumberSwap();
-      this.idCollection=[this.optionalTextSelectId,this.optionalNumberSelectId,this.inputTextId,this.inputNumberId];
-      this.defaultCollection=[this.optionalTextDefault,this.optionalNumberDefault];
+      this.idCollection = [this.optionalTextSelectId, this.optionalNumberSelectId, this.inputTextId, this.inputNumberId];
+      this.defaultCollection = [this.optionalTextDefault, this.optionalNumberDefault];
       for (let index in this.idCollection) {
-      this.defaultCollection[index]?document.getElementById(this.idCollection[index]).value = this.defaultCollection[index]:document.getElementById(this.idCollection[index]).value = "";
+        this.defaultCollection[index] ? document.getElementById(this.idCollection[index]).value = this.defaultCollection[index] : document.getElementById(this.idCollection[index]).value = "";
       }
     }
   }
